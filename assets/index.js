75b0153cdb45367ff411283dbb6d314f05a4ff20
Dann1140 <danielcan.16@gmail.com>
2022-12-05 19:11:44 -0500

    Actualizados cambios para mejorar la sintaxis

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,7 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, 
-    plugins: 
-    //[new AutoPlay()] 
-});
+const player = new MediaPlayer({ el: video });
 
 const buttonPlay = document.querySelector(".btn-play");
 const buttonMute = document.querySelector(".btn-mute");
